7012e2be04bfe3312fc0ece8766f1ec80d2b6098
anferovaav <anf0404@bk.ru>
2020-10-17 11:29:02 +0400

    =)

--- src/test/java/ru/anf/LoginTest.java
+++ src/test/java/ru/anf/LoginTest.java
@@ -1,9 +1,16 @@
 package ru.anf;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 public class LoginTest {
+
+    public static WebDriver driver;
+    public static LoginPage loginPage;
+    public static ProfilePage profilePage;
     /**
      * осуществление первоначальной настройки
      */
@@ -12,7 +19,9 @@ public class LoginTest {
         //определение пути до драйвера и его настройка
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
         //создание экземпляра драйвера
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
         //окно разворачивается на полный экран
         driver.manage().window().maximize();
         //задержка на выполнение теста = 10 сек.
@@ -20,4 +29,27 @@ public class LoginTest {
         //получение ссылки на страницу входа из файла настроек
         driver.get(ConfProperties.getProperty("loginpage"));
     }
+
+    @Test
+    public void loginTest() {
+        //значение login/password берутся из файла настроек по аналогии с chromedriver
+//и loginpage
+//вводим логин
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        //нажимаем кнопку входа
+        loginPage.clickLoginBtn();
+        //вводим пароль
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        //нажимаем кнопку входа
+        loginPage.clickLoginBtn();
+        //получаем отображаемый логин
+        String user = profilePage.getUserName();
+        //и сравниваем его с логином из файла настроек
+        Assert.assertEquals(ConfProperties.getProperty("login"), user); }
+
+    @AfterClass
+    public static void tearDown() {
+        profilePage.entryMenu();
+        profilePage.userLogout();
+        driver.quit(); }
 }
