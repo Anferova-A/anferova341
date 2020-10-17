9db1b420dd795591854e756a44c3a6d9d6007d60
anferovaav <anf0404@bk.ru>
2020-10-17 10:45:17 +0400

    =)

--- src/test/java/ru/anf/LoginTest.java
+++ src/test/java/ru/anf/LoginTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest {
    public void Setup(){
        WebDriver driver = new ChromeDriver();
-       System.setProperty("webdriver.chrome.driver", "C:\\Users\\anferovaav\\IdeaProjects");
+       System.setProperty("webdriver.chrome.driver", "C:/Users/anferovaav/IdeaProjects");
        driver.manage().window().maximize();
        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
        driver.get("https://www.citilink.ru/login/");
