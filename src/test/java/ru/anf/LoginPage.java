43b5bdf17aa83251bd86954563190b57a16aceb5
anferovaav <anf0404@bk.ru>
2020-10-17 11:08:22 +0400

    =)

--- src/test/java/ru/anf/LoginPage.java
+++ src/test/java/ru/anf/LoginPage.java
@@ -1,10 +1,22 @@
 package ru.anf;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
 
+    /**
+     * определение локаторв
+     */
     @FindBy(xpath = "//*[@id=\"content\"]/div/form/fieldset/div[1]/div/input")
     private WebElement loginField;
 
@@ -13,4 +25,20 @@ public class LoginPage {
 
     @FindBy(xpath = "//*[@id=\"content\"]/div/form/fieldset/div[2]/div/input")
     private WebElement passwdField;
+
+    /**
+     * методы
+     */
+    public void inputLogin(String login) {
+        loginField.sendKeys(login); }
+
+    public void inputPasswd(String passwd) {
+        passwdField.sendKeys(passwd); }
+
+    public void clickLoginBtn() {
+        loginBtn.click(); }
+
+
+
+
 }
